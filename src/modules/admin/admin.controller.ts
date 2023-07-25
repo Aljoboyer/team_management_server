14e5e94c334372fcc3b9e1333ae0a59bd0034298
unknown <abdul.j@medlinkjobs.com>
2023-07-25 11:21:49 +0600

    add memeber ui done

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,5 +1,11 @@
 import {Request, Response} from 'express'
 import { Team } from './createTeam.schema';
+import { IUser } from '../auth/auth.interface';
+import { ObjectId } from 'mongodb';
+import { TeamUser } from '../auth/auth.schema';
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SecretKey = "te@mM@n@gement";
 
 export const createTeamController = async (req: Request, res: Response) => {
   // console.log('Hitted', req.body)
@@ -22,15 +28,41 @@ export const createTeamController = async (req: Request, res: Response) => {
     }
   };
 
-  export const getAllTeams = async (req: Request, res: Response) => {
-  
+export const getAllTeams = async (req: Request, res: Response) => {
+    // console.log('Hitted all ', req)
+    const token = req.headers['x-access-token'];
+    // console.log('Hitted all ', token)
       try {
-        const teams = await Team.find({  }).populate('createdBy');
-    
-        res.send(teams)
+        
+        
+        if (!token) {
+          console.log('not found e dukse')
+          return res.status(401).json({ error: 'Missing token' });
+        }
+      
+        jwt.verify(token, SecretKey, async (err: any, user: any) => {
+          if (err) {
+            return res.status(403).json({ error: 'Invalid token' });
+          }
+
+          const teams = await Team.find({ createdBy: new ObjectId(user.id) }).populate('createdBy');
+          res.send(teams)
+        });
   
       } catch (error) {
         res.status(500).json({ message: "Something went wrong" });
         console.log(error);
       }
     };
+
+export const getAllUser = async (req: Request, res: Response) => {
+
+        try {
+          const userData = await TeamUser.find({  });
+          res.send(userData)
+    
+        } catch (error) {
+          res.status(500).json({ message: "Something went wrong" });
+          console.log(error);
+        }
+      };
--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -1,9 +1,10 @@
-import { createTeamController, getAllTeams } from "./admin.controller";
+import { createTeamController, getAllTeams, getAllUser } from "./admin.controller";
 
 const router = require("express").Router();
 
 
 router.post("/create-team", createTeamController);
 router.get("/get-team", getAllTeams);
+router.get("/get-all-user", getAllUser);
 
 export default router
